1aa772493fd115b0f8ddf38f35556d658c264981
Rocio <23300657@uttt.edu.mx>
2026-07-04 01:27:21 -0600

    feat: servidor eureka configurado y funcionando

---
+++ eureka-server/src/main/java/com/bookstack/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.bookstack.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // <-- ESTO ES LO ÚNICO QUE AGREGAS
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
