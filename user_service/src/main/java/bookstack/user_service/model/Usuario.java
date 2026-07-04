882e2b61fd68f4217b80665629b08ede91c01372
Rocio <23300657@uttt.edu.mx>
2026-07-04 12:35:14 -0600

    feat: creación de modelo Usuario, repositorio y autogeneración de tabla

---
+++ user_service/src/main/java/bookstack/user_service/model/Usuario.java
@@ -0,0 +1,30 @@
+package bookstack.user_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String rol; // Aquí guardaremos "ROLE_ADMINISTRADOR" o "ROLE_USUARIO"
+}
