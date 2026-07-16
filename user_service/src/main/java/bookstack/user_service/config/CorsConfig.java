602bcd62430bc3da29aabc4d34452a8caddfb96a
Rocio <23300657@uttt.edu.mx>
2026-07-16 00:21:07 -0600

    feat: agrega configuracion global de CORS en user-service

--- user_service/src/main/java/bookstack/user_service/config/CorsConfig.java
+++ user_service/src/main/java/bookstack/user_service/config/CorsConfig.java
@@ -1,4 +1,24 @@
 package bookstack.user_service.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica para todos los endpoints (como /api/v1/auth/login)
+                        .allowedOrigins("http://localhost:5173") // Le da permiso explícito a tu React
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos los encabezados
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
